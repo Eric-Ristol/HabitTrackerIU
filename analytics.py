ac066a09d931e0db891f740c621086ccb64431b1
Eric Ristol <edominguz9@gmail.com>
2025-02-21 09:37:44 +0100

    Update analytics.py

--- analytics.py
+++ analytics.py
@@ -9,55 +9,58 @@ def connection(database_name):
     db_name = database_name
 
 
-"""List of all currently tracked habits"""
-def list_habits():
+"""List of all currently tracked habits:"""
+
+def list_habits()->list:
     """
     Returns a list of all current habits from the  habits table.
     The first entry of the list is an header of what is every value of the rest of the list.
-    This list is more intended to be where to get all relevant information for the user e.g forgot active habits or the periodicity of some habit.
+    This list is more intended to be where to get all relevant information for the user e.g forgot active habits or the frequency of some habit.
     """
     connect = sqlite3.connect(f"{db_name}")
     cursor = connect.cursor()
-    query = f'''SELECT habit_name, description, periodicity, creation_date FROM habits'''
+    query = f'''SELECT habit_name, description, frequency, creation_date FROM habits'''
     cursor.execute(query)
     habits_names_list = cursor.fetchall()
-    habits_names_list = [('habit_name, description, periodicity, creation_date')] + habits_names_list
+    habits_names_list = [('habit_name, description, frequency, creation_date')] + habits_names_list
     return habits_names_list
 
 
 
-"""List of all habits with the same periodicity"""
-def same_period(periodicity):
+"""List of all habits with the same frequency"""
+
+def same_frequence_list(frequency)->list:
     connect = sqlite3.connect(f"{db_name}")
     cursor = connect.cursor()
-    query = f'''SELECT habit_name FROM habits WHERE periodicity = ?'''
-    cursor.execute(query,(periodicity,))
-    periodicity_names_list = cursor.fetchall()
-    if not periodicity_names_list:
+    query = f'''SELECT habit_name FROM habits WHERE frequency = ?'''
+    cursor.execute(query,(frequency,))
+    frequency_names_list = cursor.fetchall()
+    if not frequency_names_list:
         return False
-    return periodicity_names_list
+    return frequency_names_list
 
 
 """This functions are used for the streaks functions and struggle functions"""
 
-#For given name funcitons
-#Just return a binary list that are the checksoff of a habit.
-#This function also checks the existence of the habit to prevent further errors.
-def get_checks(habit_name:str)-> int:
-    #Look for the existence of the habit.
-    periodicity, success = db.search_habit(habit_name) #In this case periodicity is not used, but I have to creaste the variable because the function return 2 values, one is periodicity and the other is a boolean.
+
+def get_checks(habit_name)->list:
+    """
+    This funciton returns a binary list of 0s and 1s that are the checks of a habit.
+    If the habit does not exist, the function returns False, this funcitonalitiy usually was used into choices, but because of 4 choices use this single funciton I prefered to put it here to not repeat code.
+    """
+    print("get_checks executed")
+    frequency, success = db.search_habit(habit_name) #In this case frequency is not used, but I have to creaste the variable because the function return 2 values, one is frequency and the other is a boolean.
     if not success:
         print("This habit does not exist!")
         return False #If the habit is not found, the rest of the function is not executed.
+    
     connect = sqlite3.connect(f"{db_name}")
     cursor = connect.cursor()
     query = f'''SELECT checking FROM habit_{habit_name}'''
     cursor.execute(query)
     result = cursor.fetchall()  #This is a list of tuples
-    if result is None:
-        return False
     checks_list = [item[0] for item in result]  #Get only the first value of each tuple
-    return True, checks_list
+    return checks_list
 
 #For overall functions
 def only_name_list():
@@ -79,13 +82,11 @@ def only_name_list():
 
 
 #With this function we get the highest streak. Also is posible to obtain all the streak.
-def streak(habit_name)-> int:
+def streak(habit_name):
     checks_list = get_checks(habit_name)
-    if not checks_list: #This means if check list is empty.
-        return
+    if not checks_list or checks_list == False: #This means if check list is empty.
+        return False
     
-
-
     length = len(checks_list)
     count = []
     index = 0
@@ -95,7 +96,7 @@ def streak(habit_name)-> int:
         read = checks_list[index]
         if read == 1:
             count.append(1)
-        elif read == 0:
+        elif read == 0:#The appropiate syntax is else because only 0 and 1 are possible, but I wrote it in this way to make it more readable.
             if count:
                 streak.append(sum(count))
                 count = []
@@ -105,7 +106,7 @@ def streak(habit_name)-> int:
         if count:
             streak.append(sum(count))
             count = []
-    if not streak: #This checks if the list is empt, if is empty returns false, which is the problematic outocme.
+    if not streak: #This checks if the list is empty, and if it is assigns 0 to the list.
         streak = [0]
     highest_streak = max(streak)
     return highest_streak
@@ -115,17 +116,18 @@ def streak(habit_name)-> int:
 
 
 
-def overall_streak()->int:
+def overall_streak():
     habits_names_list = only_name_list()
-    habits_names_list = [item[0] for item in habits_names_list]#COnverts the list of tuples ino a list of strings  
+    habits_names_list = [item[0] for item in habits_names_list]#COnverts the list of tuples into a list of strings  
     num_habits = len(habits_names_list)
     index = 0
     overall_streak = [0]
     while index < num_habits:
         habit_name = habits_names_list[index]
-        overall_streak.append(streak(habit_name))# The [0] is because habit_name from habits_names_list returns a list with habit_name,description,periodicity and creation_date, but I only need the habit_name.
+        overall_streak.append(streak(habit_name))# The [0] is because habit_name from habits_names_list returns a list with habit_name,description,frequency and creation_date, but I only need the habit_name.
         index += 1
     longest_streak = max(overall_streak)
+    
     return longest_streak
         
 
@@ -137,10 +139,10 @@ def overall_streak()->int:
 
 
 
-def struggle(habit_name)->int:#Is just the straks function but counting 0s instead of 1s
-    if get_checks(habit_name) == False:
-        return
+def struggle(habit_name):#Is just the straks function but counting 0s instead of 1s
     checks_list = get_checks(habit_name)
+    if not checks_list or checks_list == False: #This means if check list is empty.
+        return False
 
     length = len(checks_list)
     count = []
@@ -161,7 +163,7 @@ def struggle(habit_name)->int:#Is just the straks function but counting 0s inste
         if count:
             struggle.append(sum(count))
             count = []
-    if not struggle: #This checks if the list is empt, if is empty returns false, which is the problematic outocme.
+    if not struggle: #This checks if the list is empty, if is empty returns false, which is the problematic outocme.
         struggle = [0]
     highest_struggle = max(struggle)
     return highest_struggle
@@ -172,7 +174,7 @@ def struggle(habit_name)->int:#Is just the straks function but counting 0s inste
 """- return the longest struggle of all defined habits,"""
 
 
-def overall_struggle()->int:
+def overall_struggle():
     habits_names_list = only_name_list()
     habits_names_list = [item[0] for item in habits_names_list]#COnverts the list of tuples ino a list of strings  
     num_habits = len(habits_names_list)
@@ -180,7 +182,7 @@ def overall_struggle()->int:
     overall_struggle= [0]
     while index < num_habits:
         habit_name = habits_names_list[index]
-        overall_struggle.append(struggle(habit_name))# The [0] is because habit_name from habits_names_list returns a list with habit_name,description,periodicity and creation_date, but I only need the habit_name.
+        overall_struggle.append(struggle(habit_name))# The [0] is because habit_name from habits_names_list returns a list with habit_name,description,frequency and creation_date, but I only need the habit_name.
         index += 1
     longest_struggle = max(overall_struggle)
     return longest_struggle
@@ -208,7 +210,7 @@ def module():
 
         print("\nAnalytics module....")
         print("\n1. List of all currently tracked habits")
-        print("2. List of all habits with the same periodicity")
+        print("2. List of all habits with the same frequency")
         print("\n3. Longest run streak of all defined habits")
         print("4. Longest run streak for a given habit")
         print("\n5. Longest struggle of all defined habits")
@@ -229,19 +231,18 @@ def module():
         elif choice == "2":
             while True: #Check if the input value is an integer or not, and if not asks again until an intger is introduced.
                 try:
-                    periodicity = int(input("Enter the periodicity (in days): "))
-                    if periodicity < 1:
-                        print("The periodicity has to be greater than 0!")
-                        return int(empty(str(periodicity)))#I have to transform the value into a string to manage it with the empty function because .strip is not possible with integers.Finally I transform it again into an INTEGER because the database only manages INTEGER in the periodicicty value.
+                    frequency = int(input("Enter the frequency (in days): "))
+                    if frequency < 1:
+                        print("The frequency has to be greater than 0!")
+                        return int(empty(str(frequency)))#I have to transform the value into a string to manage it with the empty function because .strip is not possible with integers.Finally I transform it again into an INTEGER because the database only manages INTEGER in the frequency value.
                     break
                 except ValueError:
                     print("This value has to be an integer!")
-            periodicity_names_list = same_period(periodicity)
-            if periodicity_names_list == False:
-                print("There are no habit with this periodicity")
-                return
-            periodicity_names_list = [(val.replace('_', ' ') if isinstance(val, str) else val) for val in periodicity_names_list[0]]
-            print(periodicity_names_list)
+            frequency_names_list = same_frequence_list(frequency)
+            if frequency_names_list == False:
+                print("There are no habit with this frequency")
+                continue
+            print(frequency_names_list)
 
 
         elif choice == "3":
@@ -249,8 +250,6 @@ def module():
             print(f"Longest streak: {longest_streak} days")
         elif choice == "4":
             habit_name = empty(input("For which habit do you want to get the longest streak?: "))
-            
-
             habit_name = habit_name.replace(" ", "_")
             highest_streak = streak(habit_name)
             print( f"{highest_streak} days")
@@ -272,4 +271,3 @@ def module():
 
         else:
             print("Invalid choice. Please enter a number between 1 and 7.")
-
