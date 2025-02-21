664085c5594a5493af4ebd0c609148b8da526bc2
Eric Ristol <edominguz9@gmail.com>
2025-02-21 21:20:05 +0100

    Update habit.py

--- habit.py
+++ habit.py
@@ -19,7 +19,6 @@ class Habit:
 
     def modify(self, habit_name, new_habit_name, new_description):
        
-        #Modify the habits table
         db.rename_habit_in_habits_table(new_habit_name, new_description, habit_name)
        
         #Modify the name of the check off table
@@ -46,7 +45,7 @@ class Habit:
 
     def check_off_date(self,habit_name,check_off_date,frequency):
 
-        check_off_date = db.compare_creation_checkoff_dates(habit_name,check_off_date) #In case the user introduces a check off date posterior to the creation date, this is solved assking to rewirte it and through this function the new valid input is transmitted to the rest of habit class.
+        check_off_date = db.compare_creation_checkoff_dates(habit_name,check_off_date) #In case the user introduces a check off date anterior to the creation date or in the future from today, this is solved asking to rewirte it and through this function the new valid input is transmitted to the rest of the function.
         db.create_intermediate_dates_checkoff_table(habit_name, check_off_date) 
         db.check_off(habit_name, check_off_date,frequency)
         db.show_check_off(habit_name,check_off_date,frequency)
